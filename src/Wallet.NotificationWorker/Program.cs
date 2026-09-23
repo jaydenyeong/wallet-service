2addd7a62661a4bc32641d204f66aa27c137bc03
jaydenyeong <jaydenyeong01@hotmail.com>
2026-09-23 21:15:48 +0800

    chore: scaffold solution and local infrastructure

---
+++ src/Wallet.NotificationWorker/Program.cs
@@ -0,0 +1,7 @@
+using Wallet.NotificationWorker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ tests/Wallet.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Wallet.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
